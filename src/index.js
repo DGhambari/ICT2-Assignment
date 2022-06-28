95c034393faacf90746739754abd3c6d768cb1ef
DGhambari <darrenghambari@yahoo.com>
2022-06-28 21:08:04 +0100

    Full review page.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
-import HomePage from "./pages/homePage";
-import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Navigate, Routes, Link } from 'react-router-dom';
+import HomePage from './pages/homePage';
+import MoviePage from './pages/movieDetailsPage';
+import FavouriteMoviesPage from './pages/favouriteMoviesPage';
+import MovieReviewPage from './pages/movieReviewPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to="/movies/favourites">Favourites</Link>
+          <Link to='/movies/favourites'>Favourites</Link>
         </li>
       </ul>
       <Routes>
-        <Route path="/movies/favourites" element={<FavouriteMoviesPage/>}
-        />
-        <Route path="/movies/:id" element={<MoviePage/>} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path='/reviews/:id' element={<MovieReviewPage />} />
+        <Route path='/movies/favourites' element={<FavouriteMoviesPage />} />
+        <Route path='/movies/:id' element={<MoviePage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
