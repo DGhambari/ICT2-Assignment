b32ae37710ef86e38337f65e68d3abde92ec5e94
DGhambari <darrenghambari@yahoo.com>
2022-07-30 17:52:15 +0100

    Fix backward/forward arrows

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -1,45 +1,47 @@
-import React from "react";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import HomeIcon from "@material-ui/icons/Home";
+import React from 'react';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import HomeIcon from '@material-ui/icons/Home';
+import { useNavigate } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    flexWrap: "wrap",
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexWrap: 'wrap',
     padding: theme.spacing(1.5),
   },
   tagLine: {
-    fontSize: "1.5rem",
+    fontSize: '1.5rem',
   },
 }));
 
 const MovieHeader = (props) => {
+  const navigate = useNavigate();
   const classes = useStyles();
   const movie = props.movie;
 
   return (
-    <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back">
-        <ArrowBackIcon color="primary" fontSize="large" />
+    <Paper component='div' className={classes.root}>
+      <IconButton aria-label='go back' onClick={() => navigate(-1)}>
+        <ArrowBackIcon color='primary' fontSize='large' />
       </IconButton>
 
-      <Typography variant="h4" component="h3">
+      <Typography variant='h4' component='h3'>
         {movie.title}
         <a href={movie.homepage}>
-          <HomeIcon color="primary" />
+          <HomeIcon color='primary' />
         </a>
         <br />
         <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
-        <ArrowForwardIcon color="primary" fontSize="large" />
+      <IconButton aria-label='go forward' onClick={() => navigate(1)}>
+        <ArrowForwardIcon color='primary' fontSize='large' />
       </IconButton>
     </Paper>
   );
